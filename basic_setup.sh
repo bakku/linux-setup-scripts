783131b8dcef612307dba39cf09ce9edc40d4102
Christian Paling <christian.paling@googlemail.com>
2016-12-21 11:29:52 +0100

    Add git default push behaviour

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,6 +1,7 @@
 sudo apt-get install git
 git config --global user.name "Christian Paling"
 git config --global user.email "christian.paling@googlemail.com"
+git config --global push.default simple
 
 cd $HOME
 mkdir -p Development/bakku
