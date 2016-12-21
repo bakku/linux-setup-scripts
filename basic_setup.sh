aacfae7ce9471511f00e89a6d758af0c9b8de9fd
Christian Paling <christian.paling@googlemail.com>
2016-12-21 11:46:12 +0100

    Add zsh configuration

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,12 +1,22 @@
+# setup git
 sudo apt-get install git
 git config --global user.name "Christian Paling"
 git config --global user.email "christian.paling@googlemail.com"
 git config --global push.default simple
 
+# create dev folder
 cd $HOME
 mkdir -p Development/bakku
 cd Development/bakku
+
+# clone dotfiles
 git clone https://github.com/bakku/dotfiles
-cp dotfiles/.vimrc $HOME
+
+# configure vim
+cp $HOME/Development/bakku/dotfiles/.vimrc $HOME
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p .vim/tmp
+
+# configure zsh
+sudo apt-get install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
