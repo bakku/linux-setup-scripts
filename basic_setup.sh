708f63ce467a333f2e1057089d8510faf6c1b6de
Christian Paling <christian.paling@googlemail.com>
2017-08-27 13:47:20 +0200

    Add more commands to make setup less error prone

--- basic_setup.sh
+++ basic_setup.sh
@@ -1,21 +1,23 @@
-# setup git
-sudo apt-get install git
+# install necessary software
+sudo apt-get -y install git curl build-essential
+
+# configure git
 git config --global user.name "Christian Paling"
 git config --global user.email "christian.paling@googlemail.com"
 git config --global push.default simple
 
 # create dev folder
-cd $HOME
-mkdir -p Development/bakku
-cd Development/bakku
+mkdir -p $HOME/Development/bakku
 
 # clone dotfiles
+mkdir -p $HOME/Development/bakku/misc
+cd $HOME/Development/bakku/misc
 git clone https://github.com/bakku/dotfiles
 
 # configure vim
-cp $HOME/Development/bakku/dotfiles/.vimrc $HOME
+cp $HOME/Development/bakku/misc/dotfiles/.vimrc $HOME
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-mkdir -p .vim/tmp
+mkdir -p $HOME/.vim/tmp
 
 # configure zsh
 sudo apt-get install zsh
